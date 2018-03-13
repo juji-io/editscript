ff9ddb6a2973dc73e4b8f0c83866e408beebf2d9
Huahai Yang <huahai.yang@gmail.com>
2018-03-13 23:27:27 +0800

    wip-1

--- src/editscript/core.clj
+++ src/editscript/core.clj
@@ -1,6 +1,6 @@
 (ns editscript.core
   (:require [clojure.set :as set])
-  (:import [clojure.lang Seqable PersistentVector]))
+  (:import [clojure.lang Seqable PersistentVector IPersistentCollection]))
 
 (set! *warn-on-reflection* true)
 
@@ -48,58 +48,17 @@
 
   Seqable
   (seq [this]
-    (.seq edits)))
+    (.seq edits))
 
-(defn diff
-  "Create an editscript that represents the difference between `b` and `a`"
-  [a b])
-
-(defn patch
-  "Apply the editscript `es` on `a` to produce `b`"
-  [a es])
-
-
-(def es (->EditScript "a" [] 0 0 0))
-(add-data es [] "b")
-
-(get-edits es)
-(get-adds-num es)
-(seq es)
-(edit-distance es)
-
-
-(def a {:a 1 :b 'b :c {:d 3}})
-(def b {:b 'c :c {:d 2} :e 5 :f 6})
-[[[:e] ::+ 5]
- [[:f] ::+ 6]
- [[:a] ::-]
- [[:c :d] ::+ 2]
- [[:b] ::+ 'c]]
-
-(def c [3 'c {:a 3} 4])
-(def d [3 'c {:b 3} 4])
-[[[2 :a] ::-]
- [[2 :b] ::+ 3]]
-
-(def e nil)
-(def f {:a 42})
-[[] ::+ {:a 42}]
-
-(def g "abc")
-(def h {:a 42})
-[[] ::+ {:a 42}]
-
-(def i ["abc" 24 {:a 42}])
-(def j [{:a 42 :b 24} 1 3])
-[[[0] ::-]
- [[0] ::-]
- [[0 :b] ::+ 24]
- [[1] ::+ 1]
- [[2] ::+ 3]]
-
-(def k {:a 42 :b ["a" "b"]})
-(def l ["a" "b" "c"])
-[[[] ::+ ["a" "b" "c"]]]
+  IPersistentCollection
+  (count [this]
+    (.count edits))
+  (cons [this o]
+    (.cons edits o))
+  (empty [this]
+    (EditScript. original [] 0 0 0))
+  (equiv [this o]
+    (.equiv edits o)))
 
 (defn get-type [v]
   (cond
@@ -116,23 +75,69 @@
 (defn- diff-map [script path a b]
   )
 
-(defn differ [script path a b]
-  (case (get-type a)
-    :nil (swap! script conj [path ::+ b])
-    :map (case (get-type b)
-           :nil (swap! script conj [path ::-])
-           :map (diff-map script path a b))
-    :vec (case (get-type b)
-           :nil (swap! script conj [path ::-]))
-    :set (case (get-type b)
-           :nil (swap! script conj [path ::-]))
-    :seq (case (get-type b)
-           :nil (swap! script conj [path ::-]))
-    :val (case (get-type b)
-           :nil (swap! script conj [path ::-]))))
+(defn diff* [script path a b]
+  (let [ta (get-type a)
+        tb (get-type b)]
+    (case ta
+     :nil (add-data script path b)
+     :map (case tb
+            :nil (delete-data script path)
+            :map (diff-map script path a b))
+     :vec (case tb
+            :nil (delete-data script path))
+     :set (case tb
+            :nil (delete-data script path))
+     :seq (case tb
+            :nil (delete-data script path))
+     :val (case tb
+            :nil (delete-data script path)))))
 
-#_(defn diff [a b]
+(defn diff
+  "Create an EditScript that represents the difference between `b` and `a`,
+  return nil if `a` and `b` are identical"
+  [a b]
   (when-not (identical? a b)
-    (let [script (atom [])
+    (let [script (->EditScript a [] 0 0 0)
           path   ^::path []]
-      (differ script path a b))))
+      (diff* script path a b))))
+
+(defn patch
+  "Apply the editscript `es` on `a` to produce `b`"
+  [a es])
+
+(comment
+  (def a {:a 1 :b 'b :c {:d 3}})
+  (def b {:b 'c :c {:d 2} :e 5 :f 6})
+  [[[:e] ::+ 5]
+   [[:f] ::+ 6]
+   [[:a] ::-]
+   [[:c :d] ::+ 2]
+   [[:b] ::+ 'c]]
+
+  (def c [3 'c {:a 3} 4])
+  (def d [3 'c {:b 3} 4])
+  [[[2 :a] ::-]
+   [[2 :b] ::+ 3]]
+
+  (def e nil)
+  (def f {:a 42})
+  [[] ::+ {:a 42}]
+  (diff e f)
+
+  (def g "abc")
+  (def h {:a 42})
+  [[] ::+ {:a 42}]
+
+  (def i ["abc" 24 {:a 42}])
+  (def j [{:a 42 :b 24} 1 3])
+  [[[0] ::-]
+   [[0] ::-]
+   [[0 :b] ::+ 24]
+   [[1] ::+ 1]
+   [[2] ::+ 3]]
+
+  (def k {:a 42 :b ["a" "b"]})
+  (def l ["a" "b" "c"])
+  [[[] ::+ ["a" "b" "c"]]]
+
+  )
