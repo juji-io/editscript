a98a982624ee8ad594dc1e296b5011de8c5c4997
Huahai Yang <huahai.yang@gmail.com>
2018-03-14 14:27:28 +0800

    map diff

--- src/editscript/core.clj
+++ src/editscript/core.clj
@@ -7,11 +7,9 @@
 (defprotocol IEdit
   (add-data [this path value])
   (delete-data [this path])
-  (replace-data [this path old new]))
+  (replace-data [this path value]))
 
 (defprotocol IEditScript
-  (diff-data [this a b])
-  (patch-data [this a diff])
   (edit-distance [this] "Report the edit instance")
   (get-edits [this] "Report the edits as a vector")
   (get-adds-num [this] "Report the number of additions")
@@ -23,6 +21,8 @@
                      ^:volatile-mutable ^long adds-num
                      ^:volatile-mutable ^long dels-num
                      ^:volatile-mutable ^long reps-num]
+  :load-ns true
+
   IEdit
   (add-data [this path value]
     (locking this
@@ -32,14 +32,12 @@
     (locking this
       (set! dels-num (inc dels-num))
       (set! edits (conj edits [path ::-]))))
-  (replace-data [this path old new]
+  (replace-data [this path value]
     (locking this
       (set! reps-num (inc reps-num))
-      (set! edits (conj edits [path ::- old] [path ::+ new]))))
+      (set! edits (conj edits [path ::-] [path ::+ value]))))
 
   IEditScript
-  (diff-data [this a b])
-  (patch-data [this a diff])
   (get-edits [this] edits)
   (get-adds-num [this] adds-num)
   (get-dels-num [this] dels-num)
@@ -62,35 +60,66 @@
 
 (defn get-type [v]
   (cond
-    (nil? v)                :nil
-    (map? v)                :map
-    (vector? v)             :vec
-    (set? v)                :set
-    (and (sequential? v)
-         (not (string? v))) :seq
-    :else                   :val))
+    (nil? v)    :nil
+    (map? v)    :map
+    (vector? v) :vec
+    (set? v)    :set
+    (list? v)   :lst
+    :else       :val))
 
 (defn path? [p] (and (vector? p) (::path (meta p))))
 
+(declare diff*)
+
 (defn- diff-map [script path a b]
+  (reduce-kv
+   (fn [_ ka va]
+     (let [path' (conj path ka)]
+       (if-some [vb (get b ka)]
+        (diff* script path' va vb)
+        (diff* script path' va nil))))
+   nil
+   a)
+  (reduce-kv
+   (fn [_ kb vb]
+     (when-not (contains? a kb)
+       (diff* script (conj path kb) nil vb)))
+   nil
+   b))
+
+(defn- diff-vec [script path a b]
+  )
+
+(defn- diff-set [script path a b]
+  )
+
+(defn- diff-lst [script path a b]
   )
 
 (defn diff* [script path a b]
   (let [ta (get-type a)
         tb (get-type b)]
     (case ta
-     :nil (add-data script path b)
-     :map (case tb
-            :nil (delete-data script path)
-            :map (diff-map script path a b))
-     :vec (case tb
-            :nil (delete-data script path))
-     :set (case tb
-            :nil (delete-data script path))
-     :seq (case tb
-            :nil (delete-data script path))
-     :val (case tb
-            :nil (delete-data script path)))))
+      :nil (add-data script path b)
+      :map (case tb
+             :nil (delete-data script path)
+             :map (diff-map script path a b)
+             (replace-data script path b))
+      :vec (case tb
+             :nil (delete-data script path)
+             :vec (diff-vec script path a b)
+             (replace-data script path b))
+      :set (case tb
+             :nil (delete-data script path)
+             :set (diff-set script path a b)
+             (replace-data script path b))
+      :lst (case tb
+             :nil (delete-data script path)
+             :lst (diff-lst script path a b)
+             (replace-data script path b))
+      :val (case tb
+             :nil (delete-data script path)
+             (replace-data script path b)))))
 
 (defn diff
   "Create an EditScript that represents the difference between `b` and `a`,
@@ -99,20 +128,23 @@
   (when-not (identical? a b)
     (let [script (->EditScript a [] 0 0 0)
           path   ^::path []]
-      (diff* script path a b))))
+      (diff* script path a b)
+      script)))
 
 (defn patch
   "Apply the editscript `es` on `a` to produce `b`"
   [a es])
 
 (comment
-  (def a {:a 1 :b 'b :c {:d 3}})
-  (def b {:b 'c :c {:d 2} :e 5 :f 6})
+
+  (def a {:a {:o 4} :b 'b})
+  (def b {:a {:o 3} :b 'c :c 42})
   [[[:e] ::+ 5]
    [[:f] ::+ 6]
    [[:a] ::-]
    [[:c :d] ::+ 2]
    [[:b] ::+ 'c]]
+  (get-edits (diff a b))
 
   (def c [3 'c {:a 3} 4])
   (def d [3 'c {:b 3} 4])
