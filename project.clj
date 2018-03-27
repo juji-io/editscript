86fe3c72ec16e46b182a5f02392c40c9511308d7
Huahai Yang <huahai.yang@gmail.com>
2018-03-26 23:43:59 -0700

    update

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "A differ for Clojure data structure"
   :url "https://github.com/juji-io/editscript"
   :lein-release {:deploy-via :clojars}
+  :deploy-repositories [["releases" :clojars]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
