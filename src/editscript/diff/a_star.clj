436c2efccfb6022c31505c00e76fb1ef3c92ab73
Huahai Yang <huahai.yang@gmail.com>
2018-03-29 17:46:23 -0700

    indexing wip

--- src/editscript/diff/a_star.clj
+++ src/editscript/diff/a_star.clj
@@ -3,12 +3,47 @@
             [clojure.data.priority-map :as p]
             [editscript.core :refer :all]))
 
+(set! *warn-on-reflection* true)
+
+(defrecord Node [path type size order value])
+
+(defn- index-map [nodes i path data]
+  (let [n  (count data)
+        i' (+ i n)]
+    (vswap! nodes conj (->Node path :map n i' data))
+    (reduce-kv
+     (fn [_ k v]
+       (index* nodes i' (conj path k) v))
+     nil
+     data)))
+
+(defn- index* [nodes i path data]
+  (println "data is " data)
+  (case (get-type data)
+    :map (index-map nodes i path data)
+    :val (vswap! nodes conj (->Node path :val 1 (inc i) data))
+    ))
+
+(defn- index
+  "Traverse data as a tree to build a vector of Node"
+  [data]
+  (let [nodes (volatile! [])]
+    (index* nodes 0 [] data)
+    nodes))
+(index {:a 3})
+
 (defn- diag [[x y]] (- y x))
 
 (defn- heuristic [cur goal]
-  (Math/abs (- (diag cur) (diag goal))))
+  (Math/abs (- ^long (diag cur) ^long (diag goal))))
 
-(defn vec-a* [a b]
+(defn- trace [came cur]
+  (loop [c cur ops '()]
+    (if-let [[prev op] (came c)]
+      (recur prev (conj ops op))
+      (vec ops))))
+
+(defn A* [a b]
   (let [gx   (count a)
         gy   (count b)
         goal [gx gy]
@@ -20,10 +55,14 @@
                  1))
         ops  (fn [[x y]]
                (cond-> []
-                 (and (< x gx)
-                      (< y gy)) (conj [(inc x) (inc y)])
-                 (< x gx)       (conj [(inc x) y])
-                 (< y gy)       (conj [x (inc y)])))]
+                 (and (< x gx) (< y gy)) (conj :r)
+                 (< x gx)                (conj :-)
+                 (< y gy)                (conj :+)))
+        dest (fn [[x y] op]
+               (case op
+                 :+ [x (inc y)]
+                 :- [(inc x) y]
+                 :r [(inc x) (inc y)]))]
     (loop [open   (p/priority-map [0 0] (heuristic [0 0] goal))
            closed #{}
            came   {}
@@ -32,20 +71,21 @@
         "failed"
         (let [[cx cy :as cur] (key (peek open))]
           (if (= cur goal)
-            [came cur]
+            (trace came cur)
             (let [{:keys [open closed came g]}
                   (reduce
-                   (fn [{:keys [open closed came g] :as m} neighbor]
-                     (if (closed neighbor)
-                       m
-                       (let [tmp-g (+ (get g cur Long/MAX_VALUE) (dist cur neighbor))]
-                         (if (>= tmp-g (get g neighbor Long/MAX_VALUE))
-                           (assoc m :open (assoc open neighbor Long/MAX_VALUE))
-                           (assoc m
-                                  :came (assoc came neighbor cur)
-                                  :g (assoc g neighbor tmp-g)
-                                  :open (assoc open neighbor
-                                               (+ tmp-g (heuristic cur goal) )))))))
+                   (fn [{:keys [open closed came g] :as m} op]
+                     (let [neighbor (dest cur op)]
+                       (if (closed neighbor)
+                         m
+                         (let [tmp-g (+ (get g cur Long/MAX_VALUE) (dist cur neighbor))]
+                           (if (>= tmp-g (get g neighbor Long/MAX_VALUE))
+                             (assoc m :open (assoc open neighbor Long/MAX_VALUE))
+                             (assoc m
+                                    :came (assoc came neighbor [cur op])
+                                    :g (assoc g neighbor tmp-g)
+                                    :open (assoc open neighbor
+                                                 (+ tmp-g (heuristic cur goal) ))))))))
                    {:open   (pop open)
                     :closed (conj closed cur)
                     :came   came
@@ -53,6 +93,8 @@
                    (ops cur))]
               (recur open closed came g))))))))
 
+(def a (vec (seq "a")))
+(def b (vec (seq "ac")))
 (def a (vec (seq "acbdeacbed")))
 (def b (vec (seq "acebdabbabed")))
-(vec-a* a b)
+(A* a b)
