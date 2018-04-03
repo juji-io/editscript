8646dd18c233e65208122a83e5656f1e9bbb9f01
Huahai Yang <huahai.yang@gmail.com>
2018-04-03 11:01:39 -0700

    postorder in index

--- src/editscript/diff/a_star.clj
+++ src/editscript/diff/a_star.clj
@@ -4,77 +4,106 @@
             [editscript.core :refer :all])
   (:import [clojure.lang PersistentHashMap]))
 
-(set! *warn-on-reflection* true)
+;; (set! *warn-on-reflection* true)
 
 (defprotocol INode
   (get-path [this] "Get the path to the node from root, a vector")
   (get-value [this] "Get the actual data")
   (get-order [this] "Get the order number of the node in indices")
-  (get-children [this] "Get the children nodes")
-  (add-child [this node] "Add a child node to children map")
-  (get-size [this] "Get the size of subtree, used to estimate cost")
-  (set-size [this s] "Set the size of subtree"))
+  (set-order [this order] "Set the order number of the node in indices")
+  (get-children [this] "Get the children nodes as a map")
+  (add-child [this node] "Add a child node to the children map")
+  (get-size [this] "Get the size of sub-tree, used to estimate cost")
+  (set-size [this s] "Set the size of sub-tree"))
 
 (deftype Node [path
                value
-               order
+               ^:volatile-mutable ^long order
                ^:volatile-mutable ^PersistentHashMap children
                ^:volatile-mutable ^long size]
   INode
   (get-path [this] path)
   (get-value [this] value)
   (get-order [this] order)
+  (set-order [this o] (set! order (long o)))
   (get-children [this] children)
   (add-child [this node]
     (set! children (assoc children (last (get-path node)) node)))
   (get-size [this] size)
   (set-size [this s] (set! size (long s))))
 
-(defmethod print-method Node [x ^java.io.Writer writer]
+(defmethod print-method Node
+  [x ^java.io.Writer writer]
   (print-method {:path     (get-path x)
                  :value    (get-value x)
                  :order    (get-order x)
-                 :children (keys (get-children x))
+                 :children (map (fn [[k v]] [k (get-order v)]) (get-children x))
                  :size     (get-size x)}
                 writer))
 
 (declare index*)
 
-(defn- index-associative [nodes path data parent]
-  (let [node (->Node path data (count @nodes) {} 1)]
-    (vswap! nodes conj node)
+(defn- index-associative
+  [nodes path data parent]
+  (let [node (->Node path data 0 {} 1)]
     (add-child parent node)
     (reduce-kv
      (fn [_ k v]
        (index* nodes (conj path k) v node))
      nil
      data)
-    (set-size node
-              (+ (get-size node)
-                 (apply + (map get-size (vals (get-children node))))))))
+    (vswap! nodes conj node)
+    (doto node
+      (set-order (count @nodes))
+      (set-size (+ (get-size node)
+                   (reduce + (map get-size (vals (get-children node)))))))))
 
-(defn- index* [nodes path data parent]
+(defn- index-value
+  [nodes path data parent]
+  (let [node (->Node path data 0 nil 1)]
+                  (add-child parent node)
+                  (vswap! nodes conj node)
+                  (set-order node (count @nodes))))
+
+(defn- index*
+  [nodes path data parent]
   (case (get-type data)
     (:map :vec) (index-associative nodes path data parent)
-    :val        (let [node (->Node path data (count @nodes) nil 1)]
-                  (add-child parent node)
-                  (vswap! nodes conj node))))
+    :val        (index-value nodes path data parent)))
 
 (defn- index
-  "Traverse data to build an indexing vector of Nodes in pre-order,
+  "Traverse data to build an indexing vector of Nodes in post-order,
   and compute the sizes of their sub-trees for cost estimation"
   [data]
-  (let [nodes (volatile! [])
-        root  (->Node [] ::root 0 {} 0)]
-    (index* nodes [] data root)
+  (let [nodes (volatile! [])]
+    (index* nodes [] data (->Node [] ::root 0 {} 0))
     @nodes))
 
-(defn- diag [[x y]] (- y x))
+(defprotocol IState
+  (global [this] "The location in the global state matrix")
+  (local [this] "The location in the local state matrix")
+  (local? [this] "Is this state local?"))
 
-(defn- heuristic [cur goal]
-  (Math/abs (- ^long (diag goal) ^long (diag cur))))
+(defn- heuristic
+  "An optimistic (never over-estimate) estimate of cost to reach goal when at (x y).
+
+  For positive goal differential (delta), the optimal editscript length is deletion
+  dependent, equals to 2p+delta, where p is number of deletions. Optimistically
+  assuming no new deletion will be needed after (x, y), the cost works out to
+  be delta-(y-x). The same logic applies to negative delta.
+
+  However, because addition/replacement requires new value to be present in
+  editscript, whereas deletion does not, we assign unit cost 2 to
+  addition/replacement, 1 to deletion."
+  [[x y] [gx gy]]
+  (let [delta (- gy gx)
+        cost  (Math/abs (- delta (- y x)))]
+    (if (< delta 0)
+      cost
+      (* 2 cost))))
 
 (defn- trace [came cur]
+  (println (str "explored: " (count came)))
   (loop [c cur ops '()]
     (if-let [[prev op] (came c)]
       (recur prev (conj ops op))
@@ -82,28 +111,28 @@
 
 (defn- explore [a b cur goal
                 {:keys [open closed came g] :as m}
-                {:keys [op co nt]}]
-  (if (closed nt)
+                {:keys [op co nb]}]
+  (if (closed nb)
     m
     (let [gc    (get g cur Long/MAX_VALUE)
           tmp-g (if (= gc Long/MAX_VALUE) gc (+ gc co))]
-      (if (>= tmp-g (get g nt Long/MAX_VALUE))
-        (assoc! m :open (assoc open nt Long/MAX_VALUE))
+      (if (>= tmp-g (get g nb Long/MAX_VALUE))
+        (assoc! m :open (assoc open nb Long/MAX_VALUE))
         (assoc! m
-                :came (assoc! came nt [cur op])
-                :g (assoc! g nt tmp-g)
-                :open (assoc open nt (+ tmp-g (heuristic cur goal))))))))
+                :came (assoc! came nb [cur op])
+                :g (assoc! g nb tmp-g)
+                :open (assoc open nb (+ tmp-g (heuristic nb goal))))))))
 
 (defn- frontier [a b gx gy [x y]]
   (let [va (get a x)
         vb (get b y)]
     (if (= va vb)
-      [{:op := :co 0 :nt [(inc x) (inc y)]}]
+      [{:op := :co 0 :nb [(inc x) (inc y)]}]
       (cond-> []
-        (< x gx)       (conj {:op :- :co 1 :nt [(inc x) y]})
+        (< x gx)       (conj {:op :- :co 1 :nb [(inc x) y]})
         (and (< x gx)
-             (< y gy)) (conj {:op :r :co 2 :nt [(inc x) (inc y)]})
-        (< y gy)       (conj {:op :+ :co 2 :nt [x (inc y)]})))))
+             (< y gy)) (conj {:op :r :co 2 :nb [(inc x) (inc y)]})
+        (< y gy)       (conj {:op :+ :co 2 :nb [x (inc y)]})))))
 
 (defn A*
   "A* algorithm, works on the indices of input data"
@@ -138,13 +167,15 @@
 (comment
 
   (def a [{:a 1 :b 2} {:c 2} [4 5 6]])
+  (def b [:b {:a 1} {:c 3} [5 7]])
   (index a)
+  (index b)
 
   (def a (vec (seq "acbdeacbed")))
   (def b (vec (seq "acebdabbabed")))
   (A* nil a b)
 
-  (def a (vec (seq "abc")))
-  (def b (vec (seq "abc")))
+  (def a (vec (seq "ab")))
+  (def b (vec (seq "bc")))
   (A* nil a b)
   )
