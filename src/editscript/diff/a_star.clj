da73626a1365f058264a7b4b21387f3fdd5f1d47
Huahai Yang <huahai.yang@gmail.com>
2018-04-09 03:09:45 -0700

    track back when encounter non-leaf node

--- src/editscript/diff/a_star.clj
+++ src/editscript/diff/a_star.clj
@@ -55,14 +55,14 @@
     (doto node
       (set-order (count @nodes))
       (set-size (+ (get-size node)
-                       (reduce + (map get-size (vals (get-children node)))))))
+                   (reduce + (map get-size (vals (get-children node)))))))
     (vswap! nodes conj node)))
 
 (defn- index-value
   [nodes path data parent]
   (let [node (->Node path data (count @nodes) nil 1)]
-                  (add-child parent node)
-                  (vswap! nodes conj node)))
+    (add-child parent node)
+    (vswap! nodes conj node)))
 
 (defn- index*
   [nodes path data parent]
@@ -75,210 +75,270 @@
   compute path, sizes of sub-trees, etc."
   [data]
   (let [nodes (volatile! [])
-        head (->Node [] ::head 0 (sorted-map) 0)]
-    (vswap! nodes conj head)
+        head (->Node [] ::head -1 (sorted-map) 0)]
     (index* nodes [] data head)
     @nodes))
 
-(defprotocol IState
-  (operator [this])
-  (cost [this])
-  (neighbor [this]))
-
-(deftype State [op co nb]
-  IState
-  (operator [_] op)
-  (cost [_] co)
-  (neighbor [_] nb))
-
-(defmethod print-method State
-  [x ^java.io.Writer writer]
-  (print-method {:op (operator x)
-                 :co (cost x)
-                 :nb (neighbor x)}
-                writer))
-
 (defn- heuristic
-  "An optimistic (never over-estimate) estimate of cost to reach goal when at (x y).
-
-  For positive goal differential (delta), the optimal number of edits is deletion
-  dependent, equals to 2p+delta, where p is number of deletions. Optimistically
-  assuming no new deletion will be needed after (x, y), the cost works out to
-  be delta-(y-x). The same logic applies to negative delta.
-
-  Because addition/replacement requires new value to be present in
-  editscript, whereas deletion does not, we assign unit cost 2 to
-  addition/replacement, 1 to deletion."
+  "An optimistic estimate of cost to reach goal when at (x y).
+  For sequences, with positive goal differential (delta), the optimal number of
+  edits is deletion dependent, equals to 2p+delta, where p is number of deletions.
+  Optimistically assuming no new deletion will be needed after (x, y), the cost
+  estimate is delta-(y-x). The same logic applies to negative delta.
+  For nested structure, multiple edits may be merged into one.
+  Also, because addition/replacement requires new value to be present in
+  editscript, whereas deletion does not, we assign estimate differently. "
   [[x y] [gx gy]]
   (let [delta (- gy gx)
         cost  (Math/abs (- delta (- y x)))]
     (if (< delta 0)
-      cost
-      (* 2 cost))))
-
-(defn- trace
-  [came cur]
-  ;; (println (str "explored: " (count came)))
-  (loop [c cur ops '()]
-    (if-let [[prev op] (came c)]
-      (recur prev (conj ops op))
-      (vec ops))))
-
-(defn- explore
-  [cur goal {:keys [open closed came g] :as m} state]
-  (println "open is" open)
-  (let [[co op nb] ((juxt cost operator neighbor) state)]
-    (if (closed nb)
-     m
-     (let [gc    (get g cur Long/MAX_VALUE)
-           tmp-g (if (= gc Long/MAX_VALUE) gc (+ gc co))]
-       (println "exploring: " state " at " cur)
-       (if (>= tmp-g (get g nb Long/MAX_VALUE))
-         (if (open nb)
-           m
-           (assoc! m :open (assoc open nb Long/MAX_VALUE)))
-         (do
-           (println "got nb" nb "with cost" tmp-g)
-           (assoc! m
-                  :came (assoc! came nb [cur op])
-                  :g (assoc! g nb tmp-g)
-                  :open (assoc open nb (+ tmp-g (heuristic nb goal))))))))))
+      (if (= cost 0) 0 1)
+      (inc cost))))
 
 (defn- value+size [node] ((juxt get-value get-size) node))
 
-(defn- frontier-local
-  [ai bi [x y] [gx gy] global]
-  (let [na      (get ai x)
-        nb      (get bi y)
-        [va sa] (value+size na)
-        [vb sb] (value+size nb)
-        ta      (get-type va)
-        tb      (get-type vb)
-        x+1     (inc x)
-        y+1     (inc y)]
-    (if (and (= ta tb) (= va vb))
-      [(->State := 0 [x+1 y+1])]
-      (cond-> []
-        (< x gx)       (conj (->State :- 1 [x+1 y]))
-        (and (< x gx)
-             (< y gy)) (conj (->State :r
-                                      (let [r (get global
-                                                   [(get-order na) (get-order nb)]
-                                                   Long/MAX_VALUE)]
-                                        (println "r " r)
-                                        (if (= r Long/MAX_VALUE) r (inc r)))
-                                      [x+1 y+1]))
-        (< y gy)       (conj (->State :+ (inc sb) [x y+1]))))))
+(defprotocol IStep
+  (operator [this] "Operator to try")
+  (current [this] "Actual start location of the step")
+  (neighbor [this] "Destination of the step")
+  ;; (trail [this] "Local breakdown of the step")
+  )
 
-(defn A*-local
-  "A* algorithm, works on the indices of input data"
-  [ai bi global]
-  (println "local ai " ai)
-  (println "local bi " bi)
-  (let [rank (fn [idx] (-> idx last get-children count))
-        goal [(rank ai) (rank bi)]
-        init [0 0]]
-    (loop [{:keys [open closed came g] :as m}
-           (transient
-            {:open   (p/priority-map init (heuristic init goal))
-             :closed (transient #{})
-             :came   (transient {})
-             :g      (transient {init 0})})]
-      (if (empty? open)
-        "failed"
-        (let [[cur cost] (peek open)]
-          (if (= cur goal)
-            {:cur cur :cost cost :trace (trace (persistent! came) cur)}
-            (recur (reduce
-                    (partial explore cur goal)
-                    (assoc! m :open (pop open) :closed (conj! closed cur))
-                    (frontier-local ai bi cur goal global)))))))))
+(deftype Step [op cu nb]
+  IStep
+  (operator [_] op)
+  (current [_] cu)
+  (neighbor [_] nb))
 
-(defn children-nodes [node] (-> node get-children vals vec))
+(defmethod print-method Step
+  [x ^java.io.Writer writer]
+  (print-method {:op (operator x)
+                 :cu (current x)
+                 :nb (neighbor x)}
+                writer))
 
-(defn- replace-cost
-  [ai bi na nb g]
-  (println "replace na " na " with " nb)
-  (let []
-    ))
+(defn- get-step
+  [step]
+  ((juxt operator current neighbor) step))
+
+(defprotocol IState
+  (local? [this] "Is this local search?")
+  (get-open [this] "Get the open priority queue")
+  (set-open [this open] "Set the open priority queue")
+  (get-closed [this] "Get the closed set")
+  (set-closed [this closed] "Set the closed set")
+  (get-came [this] "Get the succession map")
+  (set-came [this came] "Set the succession map")
+  (get-g [this] "Get the g cost map")
+  (set-g [this g] "Set the g cost map"))
+
+(deftype State [local
+                ^:volatile-mutable open
+                ^:volatile-mutable closed
+                ^:volatile-mutable came
+                ^:volatile-mutable g]
+  IState
+  (local? [_] local)
+  (get-open [_] open)
+  (set-open [this o] (set! open o) this)
+  (get-closed [_] closed)
+  (set-closed [this c] (set! closed c) this)
+  (get-came [_] came)
+  (set-came [this c] (set! came c) this)
+  (get-g [_] g)
+  (set-g [this got] (set! g got) this))
+
+(defn- get-state
+  [state]
+  ((juxt local? get-open get-closed get-came get-g) state))
+
+(defn- access-g
+  [g cur]
+  (get g cur Long/MAX_VALUE))
+
+(declare A*)
+
+(defn- child-replace-cost
+  [global na nb]
+  (let [x     (get-order na)
+        y     (get-order nb)
+        start (fn [node order] (- order (-> node get-size dec)))]
+    (- (access-g global [x y])
+       (access-g global [(start na x) (start nb y)]))))
+
+(defn- children-nodes [node] (-> node get-children vals vec))
+
+(defn- trace-local [ai' bi' came' cur]
+  )
+
+(defn- restart-replace-cost
+  [na nb]
+  (let [ai                      (conj (children-nodes na) na)
+        bi                      (conj (children-nodes nb) nb)
+        {:keys [cost cur came]} (A* ai bi {:local? true})]
+    cost))
+
+(defn- compute-cost [ai bi [x y :as cur] state step]
+  (let [[local? open closed came g] (get-state state)
+        [op [x' y' :as cu] nb]      (get-step step)
+        na                          (ai x')
+        nb                          (bi y')
+        sb                          (get-size nb)
+        gc                          (access-g g cu)]
+    (if (= gc Long/MAX_VALUE)
+      gc
+      (+ gc (case op
+              := 0
+              :- 1
+              :+ (inc sb)
+              :r (cond
+                   local?     (child-replace-cost g na nb)
+                   (= cur cu) (inc sb)
+                   :else      (restart-replace-cost na nb)))))))
+
+(defn- explore
+  [ai bi cur goal state step]
+  (let [[local? open closed came g] (get-state state)
+        [op cu nb]               (get-step step)]
+    (if (closed nb)
+      state
+      (let [tmp-g (compute-cost ai bi cur state step)]
+        (if (>= tmp-g (access-g g nb))
+          (if (open nb)
+            state
+            (set-open state (assoc open nb Long/MAX_VALUE)))
+          (doto state
+            #_(set-came state
+                        (transient (merge (persistent! came)
+                                          (trace-local ai' bi' came' cur))))
+            (set-open (assoc open nb (+ tmp-g (heuristic nb goal))))
+            (set-came (assoc! came nb [cur op]))
+            (set-g (assoc g nb tmp-g))))))))
 
 (defn- frontier
-  [ai bi [x y] [gx gy] g]
-  (let [na      (get ai x)
+  [ai bi [x y :as cur] [gx gy] state]
+  (let [local   (local? state)
+        na      (get ai x)
         nb      (get bi y)
         [va sa] (value+size na)
         [vb sb] (value+size nb)
+        x'      (- x (dec sa))
+        y'      (- y (dec sb))
+        cur-    [x' y]
+        cur+    [x y']
+        curr    [x' y']
+        g       (get-g state)
+        gc      (access-g g cur)
+        g-      (access-g g cur-)
+        g+      (access-g g cur+)
+        gr      (access-g g curr)
         x+1     (inc x)
         y+1     (inc y)]
     (cond-> []
-      (< x gx)
-      (conj (->State :- 1 [x+1 y]))
+      (and (< x gx)
+           local)       (conj (->Step :- cur [x+1 y]))
+      (and (< x gx)
+           (not local)) (conj (->Step :- (if (< g- gc) cur- cur) [x+1 y]))
+      (and (< x gx)
+           (< y gy)
+           (= va vb))   (conj (->Step := cur [x+1 y+1]))
+      (and (< x gx)
+           (< y gy)
+           (not= va vb)
+           local)       (conj (->Step :r cur [x+1 y+1]))
+      (and (< x gx)
+           (< y gy)
+           (not= va vb)
+           (not local)) (conj (->Step :r (if (< gr gc) curr cur) [x+1 y+1]))
+      (and (< y gy)
+           local)       (conj (->Step :+ cur [x y+1]))
+      (and (< y gy)
+           (not local)) (conj (->Step :+ (if (< g+ gc) cur+ cur) [x y+1])))))
 
-      (and (< x gx) (< y gy))
-      (conj
-       (if (= va vb)
-         (->State := 0 [x+1 y+1])
-         (->State :r
-                  (if (and (= (get-type va) (get-type vb)) (> sa 1))
-                    (:cost (A*-local (conj (children-nodes na) na)
-                                     (conj (children-nodes nb) nb)
-                                     g))
-                    (inc sb))
-                  [x+1 y+1])))
+(defn- initialize
+  [state cur]
+  (let [[_ open closed _ _] (get-state state)]
+    (doto state
+      (set-open (pop open))
+      (set-closed (conj! closed cur)))))
 
-      (< y gy)
-      (conj (->State :+ (inc (get-size nb)) [x y+1])))))
-
-(defn- init-g [init [gx gy]]
-  (merge {init 0}
-         (zipmap (map (fn [x] [x 0]) (range 1 (inc gx))) (repeat 1))
-         (zipmap (map (fn [x] [0 x]) (range 1 (inc gy))) (repeat 1))))
-
-(defn A*
+(defn- A*
   "A* algorithm, works on the indices of input data"
-  [ai bi]
-  (let [size (fn [idx] (-> idx last get-size))
-        goal [(size ai) (size bi)]
-        init [1 1]]
-    (println "goal " goal)
-    (loop [{:keys [open closed came g] :as m}
-           (transient
-            {:open   (p/priority-map init (heuristic init goal))
-             :closed (transient #{})
-             :came   (transient {})
-             :g      (transient (init-g init goal))})]
-      (if (empty? open)
-        "failed"
-        (let [[cur cost] (peek open)]
-          (if (= cur goal)
-            {:cur cur :cost cost :trace (trace (persistent! came) cur)}
-            (recur (reduce
-                    (partial explore cur goal)
-                    (assoc! m :open (pop open) :closed (conj! closed cur))
-                    (frontier ai bi cur goal g)))))))))
+  ([ai bi]
+   (A* ai bi {}))
+  ([ai bi {:keys [local?]}]
+   (let [goal [(count ai) (count bi)]
+         init [0 0]]
+     (loop [[open closed came g :as state]
+            (->State local?
+                     (p/priority-map init (heuristic init goal))
+                     (transient #{})
+                     (transient {})
+                     {init 0})]
+       (println "g is" (apply sorted-map (mapcat identity g)))
+       (if (empty? open)
+         "failed"
+         (let [[cur cost] (peek open)]
+           (if (= cur goal)
+             {:cur cur :cost cost :came came}
+             (recur (reduce
+                     (partial explore ai bi cur goal)
+                     (initialize state cur)
+                     (frontier ai bi cur goal state))))))))))
+
+(defn- write-script
+  [steps script]
+  (doseq [[o a b] steps]
+    (let [path  (get-path a)
+          value (get-value b)]
+      (case o
+        :- (delete-data script path)
+        :r (replace-data script path value)
+        :+ (add-data script path value)
+        nil))))
+
+(defn- trace
+  ([ai bi came cur]
+   (loop [c cur steps '()]
+     (if-let [[[x y :as prev] op] (came c)]
+       (recur prev (conj steps [op (ai x) (bi y)]))
+       steps)))
+  ([ai bi came cur script]
+   (-> (trace ai bi came cur)
+       (write-script script))))
 
 (defn diff
   "Create an EditScript that represents the minimal difference between `b` and `a`"
   [a b]
   (let [script (->EditScript [] 0 0 0)]
     (when-not (identical? a b)
-      (let [{:keys [cur cost came]} (A* a b)]
-        (trace came cur)))
+      (let [ai                 (index a)
+            bi                 (index b)
+            {:keys [cur came]} (A* ai bi)]
+        (trace ai bi (persistent! came) cur script)))
     script))
 
 (comment
 
-  (def a [[:a] [:s :t] [:u]])
-  (def b [[:s] :t :s])
+  (def a [:a [:s :t] [:u]])
+  (def b [[:s :u] :t :s])
+  (diff a b)
   (A* (index a) (index b))
   (index a)
   (index b)
 
   (def a (vec (seq "acbdeacbed")))
   (def b (vec (seq "acebdabbabed")))
-  (A* (index a) (index b))
+  (diff a b)
 
   (def a (vec (seq "ab")))
   (def b (vec (seq "bc")))
-  (A* (index a) (index b))
-  )
+  (index a)
+  (index b)
+
+  [[[0] :editscript.core/-] [[] :editscript.core/+ \c]]
+  [[[0] :editscript.core/-] [[2] :editscript.core/r \c]]
+  (diff a b)
+  [[[0] :editscript.core/-] [[1] :editscript.core/+ \c]]
+  [[[0] :editscript.core/-] [[1] :editscript.core/r \c]]
+  (editscript.diff.base/diff a b))
