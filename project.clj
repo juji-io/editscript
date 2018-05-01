b155bf92a35fd803be043ed5c55968426e8b819a
Huahai Yang <huahai.yang@gmail.com>
2018-05-01 00:06:44 -0700

    remove unnecessary deps for a library release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject juji/editscript "0.2.0"
+(defproject juji/editscript "0.2.1"
   :description "A diffing library for Clojure data structures"
   :url "https://github.com/juji-io/editscript"
   :lein-release {:deploy-via :clojars}
@@ -6,6 +6,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/test.check "0.9.0"]
-                 [criterium "0.4.4"] 
-                 ])
+                 [org.clojure/test.check "0.9.0"]])
