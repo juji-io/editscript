7aa4f2f8a7cdaf08e0f936437069dc168f74940c
Huahai Yang <huahai.yang@gmail.com>
2018-05-15 00:45:10 -0700

    cleanup

--- test/editscript/test.cljs
+++ test/editscript/test.cljs
@@ -2,12 +2,10 @@
   (:require  [doo.runner :refer-macros [doo-tests]]
              editscript.core-test
              editscript.util.pairing-test
-             editscript.util.common-test
              editscript.diff.a-star-test
              editscript.diff.quick-test))
 
 (doo-tests 'editscript.util.pairing-test
-           'editscript.util.common-test
            'editscript.diff.a-star-test
            'editscript.diff.quick-test
            'editscript.core-test)
