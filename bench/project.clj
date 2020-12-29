4cc347d1580897e1780c880c782793ac976cef8e
Huahai Yang <huahai.yang@gmail.com>
2020-12-28 16:17:02 -0800

    output results as csv files

--- bench/project.clj
+++ bench/project.clj
@@ -1,5 +1,5 @@
-(defproject bench "0.1.0"
-  :description "Compare diffing alternatives"
+(defproject bench "0.2.0"
+  :description "Compare diff library alternatives"
   :url "https://github.com/juji-io/editscript"
   :lein-release {:deploy-via :clojars}
   :license {:name "Eclipse Public License"
@@ -7,8 +7,9 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/test.check "1.1.0"]
+                 [org.clojure/data.csv "1.0.0"]
                  [criterium "0.4.6"]
                  [com.taoensso/nippy "3.1.1"]
                  [differ "0.3.3"]
                  [lambdaisland/deep-diff2 "2.0.108"]
-                 [juji/editscript "0.5.1"]])
+                 [juji/editscript "0.5.3"]])
