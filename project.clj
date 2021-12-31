a084eb54e2172b7d340bbe8db9e7f95d0d986359
Huahai Yang <huahai.yang@gmail.com>
2021-12-30 22:35:24 -0800

    deploy

--- project.clj
+++ project.clj
@@ -2,7 +2,10 @@
   :description "A diff library for Clojure/ClojureScript data structures"
   :url "https://github.com/juji-io/editscript"
   :lein-release {:deploy-via :clojars}
-  :deploy-repositories [["releases" :clojars]]
+  :deploy-repositories [["clojars" {:url           "https://repo.clojars.org"
+                                    :username      :env/clojars_username
+                                    :password      :env/clojars_password
+                                    :sign-releases false}]]
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
