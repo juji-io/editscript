0692e0e7c32b6202af8d5353fecd56ac2a9af95c
Huahai Yang <huahai.yang@gmail.com>
2026-08-17 11:47:56 -0700

    release 0.8.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject juji/editscript "0.7.0"
+(defproject juji/editscript "0.8.0"
   :description "A diff library for Clojure/ClojureScript data structures"
   :url "https://github.com/juji-io/editscript"
   :lein-release {:deploy-via :clojars}
